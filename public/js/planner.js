d756bae42080d3ccc1e7eba826d2be5075225c2b
Shawn Tuttle <email": null>
2016-08-16 09:36:28 -0700

    Ajax new todo form to append list on button click

--- public/js/planner.js
+++ public/js/planner.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   showNewTodoForm();
   // var doneStructuring = false
   $("#btn-structure").on('click', function() {showTodoLists();});
-
+  submitNewTodo();
 });
 
 var draggableBoxes = function(){
@@ -19,7 +19,7 @@ var draggableBoxes = function(){
 
 var resizableBoxes = function(){
   $(".box").resizable({
-    grid: [0, 25],
+    grid: [0, 15],
     maxHeight: 400,
     maxWidth: 350,
     minHeight: 25,
@@ -54,30 +54,39 @@ var showTodoLists = function(){
     // var url = '/todos/'
   // })
 
-var showNewTodoForm = function() {
-  $("#dialog").dialog({
-      autoOpen: false,
-      minHeight: 250,
-      position: {
-                  my: "right center",
-                  at: "right center"
-               }
-    });
-  $("#button").on('click', function(){
-    event.preventDefault();
-    $("#dialog").dialog("open");
-  });
-};
+  var showNewTodoForm = function() {
+    $("#showTodoForm").on('click', function(event) {
+      event.preventDefault();
+      $("#dialog").show();
+      $("#showTodos").append($("#dialog"))
+    })
+  }
 
-
-  //   $.ajax({
-  //     // url: url,
-  //     // type: "PUT"
-
+  // var submitNewTodo = function() {
+  //   $("#dialog").on('submit', function(event) {
+  //     event.preventDefault();
+  //     url
   //   })
-  // } else {
-
   // }
+
+// Pops up jqueryui dialog box. I don't like how it looks:
+// var showNewTodoForm = function() {
+//   $("#dialog").dialog({
+//       autoOpen: false,
+//       minHeight: 250,
+//       position: {
+//                   my: "right center",
+//                   at: "right center"
+//                }
+//     });
+//   $("#button").on('click', function(){
+//     event.preventDefault();
+//     $("#dialog").dialog("open");
+//   });
+// };
+
+
+
 
 // click to clear checked items
 
